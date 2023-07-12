a1bb2ec40795936e74e2edc5f1fafff344895629
dev8 <dev8.sofona@gmail.com>
2023-07-13 00:18:26 +0300

    clean

--- app/common/components/Entity.php
+++ app/common/components/Entity.php
@@ -6,29 +6,12 @@ namespace app\common\components;
 abstract class Entity
 {
 
-    const TABLE = '';
-
-    protected $id;
-
-
     abstract public static function fields(): array;
 
     
     public function __construct($attributes = [])
     {
         $this->init($attributes);
-    }
-
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-
-    public static function getTableName(): string
-    {
-        return static::TABLE;
     }
 
 
